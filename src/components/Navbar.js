9e0db02337b09dd7af71ef07dbf8a94876b4a447
Ralph Rosael <ralphrosael27@gmail.com>
2024-10-13 13:55:15 +0800

    Update Navbar: Replace Star the Repo button with GitHub icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
     const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -22,30 +23,16 @@ const Navbar = () => {
     return (
         <nav className={`p-4 ${isDarkTheme ? 'bg-gray-800' : 'bg-blue-500'}`}>
             <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-white text-2xl sm:text-xl md:text-2xl">NFT Artworks</h1>
+                <h1 className="text-white text-2xl">NFT Artworks</h1>
                 <div className="flex items-center space-x-4">
                     {/* Star the Repo Button */}
                     <a
                         href="https://github.com/coder-ralph/NFT-Artworks"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="flex items-center space-x-2 text-sm text-white hover:text-yellow-300 transition-colors duration-300 sm:text-xs"
+                        className="flex items-center text-white hover:text-yellow-300 transition-colors duration-300"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="18"
-                            height="18"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-star"
-                        >
-                            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-                        </svg>
-                        <span className="text-sm sm:text-xs">Star the repo</span>
+                        <FaGithub className="h-5 w-5" />
                     </a>
                     {/* Theme Toggle Button */}
                     <button
