a19dfdc5c790f0542337ce0437b655a2b6a23bf4
Ralph Rosael <ralphrosael27@gmail.com>
2024-10-15 15:48:08 +0800

    feat: Integrate Vercel Web Analytics and update index.html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import HomePage from './HomePage';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-    return <HomePage />;
+    return (
+        <>
+            <HomePage />
+            <Analytics />
+        </>
+    );
 }
 
 export default App;
